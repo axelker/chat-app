cd3c5c371c145378ed9adb8181f967208b8c5b43
Kermezian Axel <68741077+axelker@users.noreply.github.com>
2022-08-25 19:38:46 +0200

    Chat-app-local

---
+++ chat-app/src/main/java/com/chatapp/chatapp/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.chatapp.chatapp.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+	
+	/*@GetMapping("/")
+	public String home(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}*/
+
+}
